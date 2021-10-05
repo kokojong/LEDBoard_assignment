0287b703fa36e565380238d96faa5825540259d6
kokojong <duwhd0349@kookmin.ac.kr>
2021-10-05 09:31:37 +0900

    print hi

--- LEDBoard_assignment/TestViewController.swift
+++ LEDBoard_assignment/TestViewController.swift
@@ -11,6 +11,8 @@ class TestViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        print("hi")
 
         // Do any additional setup after loading the view.
     }
